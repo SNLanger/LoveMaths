5739e141f865c755f7d00adfad3a7b837d0afcdc
SNLanger <samuelnathanlanger@gmail.com>
2024-01-30 14:59:01 +0000

    try again

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,9 +17,8 @@ rungame("addition");
 function rungame(gameType) {
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
-    if(gametype==="addition") {
+    if(gametype ==="addition") {
         displayAdditionQuestion(num1, num2);
-    
     } else{
         alert('Unknown game type: ${gameType}')
         throw 'Unknown game type: ${gameType}. Aborting!'
@@ -40,8 +39,6 @@ function incrementWrongAnswer(operand1, operand2){
 document.getElementById('operand1').textContent = operand1;
 document.getElementById('operand2').textContent = operand2;
 document.getElementById('operator').textContent = "+"; 
-
-document
 }
 function displayAdditionQuestion(){
 
